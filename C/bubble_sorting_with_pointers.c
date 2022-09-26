d064897f598e92eb8dbb1b3267c75b3a5a647f38
YourShadow <shadow@MacBook-Air-2098.local>
2022-09-26 09:49:05 +0400

    Files renamed and added

---
+++ C/bubble_sorting_with_pointers.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int bubble_sorting(int *arr, int n);
+
+int main() {
+
+	int arr[] = {-34, 4, -55, 38, -8, -44, 64, 27, 48, -70};
+	bubble_sorting(arr, sizeof(arr) / sizeof(int));
+
+
+	return 0;
+}
+
+int bubble_sorting(int *arr, int n) {
+
+	int lenght = 0;
+
+	// int arr[] = {-34, 4, -55, 38, -8, -44, 64, 27, 48, -70};
+
+
+	// lenght = sizeof(arr) / sizeof(int);
+    lenght = n;
+	printf("%d\n", lenght);
+
+	printf("size is %d\n", lenght);
+
+	int i;
+	int j;
+	for (i=0; i < lenght; i++) {
+		//printf("%d\n", i);
+		for (j=0; j < lenght-1; j++) {
+			//printf("%d\n", j);
+
+			if (arr[j] > arr[j + 1]) {
+				int a = arr[j];
+				int b = arr[j + 1];
+
+				arr[j] = b;
+				arr[j + 1] = a;
+
+			}
+		}
+
+	}
+
+	for (i=0; i < lenght; i++) {
+		printf("%d\n", arr[i]);
+	}
+
+	return 0;
+
+	}

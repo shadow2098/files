4f36dadbbd4a6c91a2263c845d95a0fb629f6e0e
YourShadow <shadow@MacBook-Air-2098.local>
2022-12-31 13:06:09 +0400

    .

--- C/deletion_of_element.c
+++ C/deletion_of_element.c
@@ -7,10 +7,26 @@
 #include <stdio.h>
 #include <time.h>
 
-int main {
-    srand(time(NULL));
+int main() {
+    //srand(time(NULL));
+    int index;
+    printf("Insert index: ");
+    scanf("%d", &index);
 
     int num_arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    for (int i = 0; i < 10;)
+    for (int i = index; i < 10 - 1; i++) {
+        num_arr[i] = num_arr[i + 1];
+    }
+
+
+    for (int i = 0; i < 10; i++) {
+        printf("%d\n", num_arr[i]);
+    }
+    /*
+    for (int i; i = 0; i < 10; i++) {
+        printf("%d ",num_arr[i]);
+    }
+    */
+    return 0;
 }

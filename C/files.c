eb2dec8419f07c6d81fa5f22db6dd8fb8a915774
YourShadow <shadow@MacBook-Air-2098.local>
2023-03-03 23:43:23 +0400

    C files added

---
+++ C/files.c
@@ -0,0 +1,25 @@
+#include "stdio.h"
+
+int main() {
+    FILE *f, *w, *v;
+    char text[100];
+
+    f = fopen("test1.txt", "w");
+    fprintf(f, "Hello World.");
+    fclose(f);
+
+    w = fopen("test1.txt", "r");
+    fgets(text, 100, w);
+    printf("%s\n", text);
+    fclose(w);
+
+    v = fopen("test1.txt", "a");
+    fprintf(v, " Apple orange");
+    fclose(v);
+
+    w = fopen("test1.txt", "r");
+    fgets(text, 100, w);
+    printf("%s\n", text);
+    fclose(w);
+
+}

dc70fe7780fbfb7a5f59f8d162b674bd90be3cb8
YourShadow <shadow@MacBook-Air-2098.local>
2023-03-06 20:16:06 +0400

    Dynamic matrix modified

--- C/dy_matrix.c
+++ C/dy_matrix.c
@@ -2,9 +2,10 @@
 #include "stdio.h"
 #include <time.h>
 
+
 struct Results{
     int Max;
-    int** Avg_Arr;
+    int* Avg_Arr;
 }; typedef struct Results res;
 
 
@@ -28,10 +29,25 @@ int main() {
 
     fill_matrix(matrix, rows, cols);
 
-    // print_matrix(matrix, rows, cols);
+    print_matrix(matrix, rows, cols);
 
-    find_avg_and_max(matrix, rows, cols);
 
+    res res1;
+    res1 = find_avg_and_max(matrix, rows, cols);
+
+    if (res1.Max == -1) {
+        return -1;
+    }
+
+    printf("Max - %d\n", res1.Max);
+
+
+    // int arr_len = (int) sizeof(res1.Avg_Arr) / sizeof(int);
+    for (int i = 0; i < 20; ++i) {
+        printf("Avg arr %d - %d\n", i + 1, res1.Avg_Arr[i]);
+    }
+
+    free(res1.Avg_Arr);
     free_matrix(matrix, rows);
 }
 
@@ -85,28 +101,38 @@ void print_matrix(int** matrix, int rows, int cols) {
     printf("\n");
 }
 
+
 res find_avg_and_max(int** matrix, int rows, int cols) {
-    int y = 1;
+    
+    res res1;
     int max = 0;
     int avg = 0;
-    int** avg_arr = create_matrix(y, cols);
+    int* avg_arr = (int*) malloc(cols * sizeof(int));
+
+
+    if (sizeof(res1.Avg_Arr) != sizeof(NULL)) {
+        res1.Max = -1;
+        return res1;
+    }
 
     for (int j = 0; j < cols; ++j) {
         for (int i = 0; i < rows; ++i) {
             avg += matrix[i][j];
+            printf("%d", i);
         }
-
+        printf("\n");
+        printf("Max - %d || Avg - %d\n", max, avg);
+        avg_arr[j] = avg / 10;
         if (avg > max) {
             max = avg;
         }
-        avg_arr[y][j] = avg / 10;
+
         avg = 0;
     }
 
-    res res1;
+    
     res1.Max = max;
     res1.Avg_Arr = avg_arr;
 
-    free_matrix(avg_arr, y);
     return res1;
 }

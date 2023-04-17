2e3df94ee180b409a507dcf7441ccd0b6a84fa8b
YourShadow <shadow@MacBook-Air-2098.local>
2023-04-17 20:22:25 +0400

    str_cmp.c modified

--- C/str_cmp.c
+++ C/str_cmp.c
@@ -5,26 +5,28 @@ int compare_str(char* str, char* str1);
 
 
 int main() {
-	char str[] = "abcd\0";
-	char str1[] = "abcdef";
+	char str1[] = "abcd";
+	char str2[] = "a\0bcd";
 	int res, res1;
 
-	res = strcmp(str, str1);
-	printf("real strcmp(str, str1) = %d\n", res);
+	res = strcmp(str1, str2);
+	printf("real strcmp(str1, str2) = %d\n", res);
 
-	res1 = compare_str(str, str1);
-	printf("artificial compare_str(str, str1) = %d\n", res1);
+	res1 = compare_str(str1, str2);
+	printf("artificial compare_str(str1, str2) = %d\n", res1);
 
 	return 0;
 }
 
 
-int compare_str(char* str, char* str1) {
+int compare_str(char* str1, char* str2) {
 	int i = 0;
-	while (str[i] == str1[i]) {
+	while ((str1[i] != '\0') || (str2[i] != '\0')) {
+		if (str1[i] != str2[i]) {
+			int res = (int) str1[i] - (int) str2[i];
+			return res;
+		}
 		++i;
 	}
-	int res = (int) str[i] - (int) str1[i];
-
-	return res;
+	return 0;
 }

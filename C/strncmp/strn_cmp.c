bcb3ce80b81fdb74868871eb821135724844bbf3
YourShadow <shadow@MacBook-Air-2098.local>
2023-05-16 19:53:40 +0400

    strncmp modified

--- C/strncmp/strn_cmp.c
+++ C/strncmp/strn_cmp.c
@@ -14,38 +14,71 @@
 			что эту заданную последовательность символов 
 				можно менять в коде)
 */
-void read_file(char* file_name, char* text);
+void print_matrix(char** arr, int rows, int cols);
+void read_file(char* file_name, char** arr);
+char** create_matrix(int rows, int cols);
+void free_matrix(char** arr, int rows);
 
 
 int main() {
+    int rows = 10;
+    int cols = 20;
+    char** arr_user = create_matrix(rows, cols);
 
-	char file_name[20];
-	get_variable(file_name);
-	// printf("%s\n", file_name);
+    char file_name[20];
+    get_variable(file_name);
+    
+    read_file(file_name, arr_user);
 
+    print_matrix(arr_user, rows, cols);
 
-	char text[100];
-	read_file(file_name, text);
+    free_matrix(arr_user, rows);
 
-
-	printf("%s\n", text);
-
-	return 0;
+    return 0;
 }
 
-void read_file(char* file_name, char* text) {
-	FILE *f = fopen(file_name, "r");
+void read_file(char* file_name, char** arr_user) {
+    FILE *f = fopen(file_name, "r");
+
+    if (f == NULL) {
+        printf("Unable to open file!\n");
+    }
+    else {
+        int i = 0;
+        char text[20];
+        while (fgets(text, 20, f) != NULL) {
+            arr_user[i] = text;
+            ++i;
+        }
+    }
+    fclose(f);
+}
 
 
-	// char text[255];
-	if (f == NULL) {
-		printf("Unable to open file!\n");
-	}
-	else {
-		while (fgets(text, 100, f) != NULL) {
-			printf("%s", text);
-		}
-	}
+char** create_matrix(int rows, int cols) {
+    char** arr = (char**) malloc(rows * sizeof(char));
 
-	fclose(f);
+    for (int i = 0; i < rows; ++i) {
+        arr[i] = (char*) malloc(cols * sizeof(char));
+    }
+    return arr;
+}
+
+
+void print_matrix(char** arr, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        printf("\n");
+        for (int j = 0; j < cols; j++) {
+            printf("%c ", arr[i][j]);
+        }
+    }
+    printf("\n");
+}
+
+
+void free_matrix(char** arr, int rows) {
+    for (int i = 0; i < rows; i++) {
+        free(arr[i]);
+    }
+    free(arr);
 }
--- C/strncmp/test.c
+++ C/strncmp/test.c
@@ -2,23 +2,68 @@
 #include <string.h>
 #include <stdlib.h>
 
+void print_matrix(char** matrix, int rows, int cols);
+char** create_matrix(int rows, int cols);
+void free_matrix(char** arr, int rows);
+
 int main() {
-	char arr_db[10][20] = {"User1", "User2", "user3", "User4", "User" 
+    int rows = 10;
+    int cols = 20;
+    char** arr = create_matrix(rows, cols);
+
+    print_matrix(arr, rows, cols);
+
+    free_matrix(arr, rows);
+
+    return 0;
+}
+
+/*
+	char arr_db[10][20] = {"User1", "User2", "user3", "User4", "User",
 							"user5", "user6", "User7", "Admin", "NONE"};
 
-	char arr_error[10][30];
-	char flag[4] = "User";
-
+	char arr_error[10][20];
+	char flag[] = "User";
 
 	for (int i = 0; i < 10; i++) {
 		if (strncmp(arr_db[i], flag, 4) == 0) {
 			strcpy(arr_error[i], arr_db[i]);
-		}
-		// printf("%s\n", arr_db[i]);
+		} else {
+            arr_error[i][0] = '\0';
+        }
 	}
 
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < 10; i++) {
 		printf("%s\n", arr_error[i]);
 	}
 	return 0;
+}
+*/
+
+char** create_matrix(int rows, int cols) {
+    char** arr = (char**) malloc(rows * sizeof(char));
+
+    for (int i = 0; i < rows; ++i) {
+        arr[i] = (char*) malloc(cols * sizeof(char));
+    }
+    return arr;
+}
+
+
+void print_matrix(char** arr, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        printf("\n");
+        for (int j = 0; j < cols; j++) {
+            printf("%c ", arr[i][j]);
+        }
+    }
+    printf("\n");
+}
+
+
+void free_matrix(char** arr, int rows) {
+    for (int i = 0; i < rows; i++) {
+        free(arr[i]);
+    }
+    free(arr);
 }
